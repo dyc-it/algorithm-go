3e287e250d412ccbbd46bc86fc2d7538df18becd
邓远聪 <yuancong.deng@ele.me>
2016-08-03 11:20:22 +0800

    queue by container/list package in golang

---
+++ adt/node.go
@@ -0,0 +1,5 @@
+package adt
+
+type Node struct {
+	Value int
+}
---
+++ adt/queue/queue_test.go
@@ -0,0 +1,49 @@
+package queue
+
+import (
+	"testing"
+	"algorithm-go/adt"
+)
+
+
+func TestQueue_add(t *testing.T) {
+	node1 := adt.Node{Value:1}
+	node2 := adt.Node{Value:2}
+	node3 := adt.Node{Value:3}
+
+	q := MakeQueue()
+
+	q.add(&node1)
+	q.add(&node2)
+	q.add(&node3)
+
+
+	if q.first().Value != node1.Value {
+		t.Errorf("Testing fails: first value error")
+	}
+
+	if q.last().Value != node3.Value {
+		t.Errorf("Testing fails: last value error")
+	}
+}
+
+func TestQueue_delete(t *testing.T) {
+	node1 := adt.Node{Value:1}
+	node2 := adt.Node{Value:2}
+	node3 := adt.Node{Value:3}
+
+	q := MakeQueue()
+
+	q.add(&node1)
+	q.add(&node2)
+	q.add(&node3)
+	q.delete()
+
+	if q.first().Value != node1.Value {
+		t.Errorf("Testing fails: first value error")
+	}
+
+	if q.last().Value != node2.Value {
+		t.Errorf("Testing fails: last value error")
+	}
+}
